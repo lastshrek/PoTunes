c6258089e34fb23d6cab6f9fa4df0076f9e4a05b
Purchas <lastshrek@foxmail.com>
2017-03-24 14:52:54 +0800

    bug fix

--- PoTunes/播放器页/V/TrackLabel.swift
+++ PoTunes/播放器页/V/TrackLabel.swift
@@ -11,49 +11,30 @@ import UIKit
 class TrackLabel: UILabel {
 
 	override init(frame: CGRect) {
-		
 		super.init(frame: frame)
-		
 		self.font = UIFont(name: "BebasNeue", size: 16)
-		
 		self.textColor = UIColor.white
-		
 		self.textAlignment = .center
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
-		
 		fatalError("init(coder:) has not been implemented")
-	
 	}
 	
 	override func drawText(in rect: CGRect) {
-	
 		let shadowOffset: CGSize = self.shadowOffset
-		
 		let textColor = self.textColor
-		
 		let c: CGContext = UIGraphicsGetCurrentContext()!
-		
 		c.setLineWidth(1)
-		
 		c.setLineJoin(.round)
-		
 		c.setTextDrawingMode(.stroke)
-		
 		self.textColor = UIColor.white
 		
 		super.drawText(in: rect)
-		
 		c.setTextDrawingMode(.fill)
-		
 		self.textColor = textColor
-		
 		self.shadowOffset = CGSize(width: 0, height: 0)
-		
 		super.drawText(in: rect)
-		
 		self.shadowOffset = shadowOffset
 	}
-
 }
